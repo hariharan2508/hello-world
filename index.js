a53de91f1c132235aa4bb8eed8a3b2ec515da868
hariharan2508 <rms.hariharan@gmail.com>
2026-06-02 10:44:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Render!");
+  res.send("Hello from Server!");
 });
 
 const PORT = process.env.PORT || 3000;
